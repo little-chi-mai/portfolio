b206f71a92224badcdd734df80c09c950503083a
Mai Nguyen <chimai.dk@gmail.com>
2021-08-18 02:34:49 +1000

    fix scrolling behavior and added jquery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-// sticky nav bar
-let prevScrollpos = window.pageYOffset;
-console.log("OUTSIDE prevScrollpos", prevScrollpos);
 
-// let baymaxAppear = false
-// let baymaxAppear = false
-// let baymaxAppear = false
-// let baymaxAppear = false
-
+// where to store the variables if the image appears
 let imageAppear = {};
 
 const addClass = function(num, animationClass, className) {
@@ -16,7 +9,6 @@ const addClass = function(num, animationClass, className) {
   // console.log("EL POS", elPos);
 
   if (elPos < num && !imageAppear[className]) {
-    // console.log("REACHED");
     imageAppear[className] = true;
     el.classList.add(animationClass);
   }
@@ -30,7 +22,6 @@ const addClassMultiple = function(num, animationClass, classMain) {
   // console.log("classes");
 
   if (elPos < num && !imageAppear[classMain]) {
-    console.log("REACHED");
     imageAppear[classMain] = true;
     el.classList.add(animationClass);
     // classes.map(className => {
@@ -48,61 +39,77 @@ const addClassMultiple = function(num, animationClass, classMain) {
   }
 }
 
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
+$(document).ready(function() {
+  // store position of scroll
+  let scrollHistory = [1, 2, 3, 4, 5, 6];
+
+  $(window).scroll(function() {
+    
+    addClass(700, "animation-bottom", "baymax");
+    addClassMultiple(700, "animation-angle-wait", "arrow");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-0");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-2");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-1");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-3");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-4");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-5");
+    addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-6");
+    
+    let currentPos = $(window).scrollTop();
+
+    // update scrollHistory
+    scrollHistory.shift();
+    scrollHistory.push(currentPos);
+
+    // keep track of scroll if users scroll down or up
+    let scrollDownPx = scrollHistory[5] - scrollHistory[0];
+
+    if (scrollDownPx >= 7) {
+      $("#navbar").css("top", "-200px");
+    } else if (scrollDownPx <= -7) {
+      $("#navbar").css("top", "0");
+    }
+
+    if (currentPos === 0) {
+      $("#navbar").css("top", "0");
+    }
+  });
+  let myHeaders = new Headers();
+  myHeaders.append('permissions-policy', 'interest-cohort=()');
   
-  addClass(700, "animation-bottom", "baymax");
-  addClassMultiple(700, "animation-angle-wait", "arrow");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-0");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-2");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-1");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-3");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-4");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-5");
-  addClassMultiple(700, "animation-bottom-wait-3", "projects__cards-title-6");
-
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-200px";
+  // appear baymax
+  
+  
+  // Copy to clipboard
+  const copyToClipboard = function() {
+    let txt = document.createTextNode('chimai.dk@gmail.com');
+    let m = document;
+    let w = window;
+    let b = m.body;
+    b.appendChild(txt);
+    if (b.createTextRange) {
+      let d = b.createTextRange();
+      d.moveToElementText(txt);
+      d.select();
+      m.execCommand('copy');
+    } 
+    else {
+      let d = m.createRange();
+      let g = w.getSelection;
+      d.selectNodeContents(txt);
+      g().removeAllRanges();
+      g().addRange(d);
+      m.execCommand('copy');
+      g().removeAllRanges();
+    }
+    txt.remove();
+    // window.alert("Copied email to your clipboard!");
+  
+    let notiEl = document.getElementsByClassName("noti-box")[0];
+    notiEl.classList.add("visible");
+  
+    setTimeout(() => {
+      notiEl.classList.remove("visible");
+    }, 2000)
   }
-  prevScrollpos = currentScrollPos;
-}
-let myHeaders = new Headers();
-myHeaders.append('permissions-policy', 'interest-cohort=()');
-
-// appear baymax
-
-
-// Copy to clipboard
-const copyToClipboard = function() {
-  let txt = document.createTextNode('chimai.dk@gmail.com');
-  let m = document;
-  let w = window;
-  let b = m.body;
-  b.appendChild(txt);
-  if (b.createTextRange) {
-    let d = b.createTextRange();
-    d.moveToElementText(txt);
-    d.select();
-    m.execCommand('copy');
-  } 
-  else {
-    let d = m.createRange();
-    let g = w.getSelection;
-    d.selectNodeContents(txt);
-    g().removeAllRanges();
-    g().addRange(d);
-    m.execCommand('copy');
-    g().removeAllRanges();
-  }
-  txt.remove();
-  // window.alert("Copied email to your clipboard!");
-
-  let notiEl = document.getElementsByClassName("noti-box")[0];
-  notiEl.classList.add("visible");
-
-  setTimeout(() => {
-    notiEl.classList.remove("visible");
-  }, 2000)
-}
+})
